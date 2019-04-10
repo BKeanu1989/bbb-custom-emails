b4dc748b75133bd5bd6a1e0dd7def9ddc537d899
Kevin Fechner <developer.kevinfechner@gmail.com>
2019-04-10 11:38:19 +0200

    change package name

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ gulp.task('pot', function () {
     return gulp.src('./**/*.php')
         .pipe(wpPot( {
             domain: 'bbb-custom-emails',
-            package: 'StoreFront Child'
+            package: 'BigBoxBerlin Custom Emails'
         } ))
         .pipe(gulp.dest('./languages/bbb-custom-emails.pot'));
 });
