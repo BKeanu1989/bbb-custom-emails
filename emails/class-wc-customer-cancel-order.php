0797a8b1f457957d24ac9373415cd3fb1f98996c
Kevin Fechner <developer.kevinfechner@gmail.com>
2019-05-30 23:55:13 +0200

    actual status

--- emails/class-wc-customer-cancel-order.php
+++ emails/class-wc-customer-cancel-order.php
@@ -18,7 +18,8 @@ class WC_BBB_Customer_Cancel_Order extends WC_Email
      */
     public function __construct()
     {
-        // Email slug we can use to filter other data.
+		// Email slug we can use to filter other data.
+		// FIXME: notice: changed from wc_customer_cancelled_order -> customer_cancelled_order
         $this->id = 'wc_customer_cancelled_order';
         $this->title = __('Stornierte Bestellungsemail', 'bbb-custom-emails');
         $this->description = __('Email bei stornierter Bestellung.', 'bbb-custom-emails');
@@ -52,19 +53,20 @@ class WC_BBB_Customer_Cancel_Order extends WC_Email
 	 * @return void
 	 */
 	function trigger( $order_id, $order = false) {
+
 		$this->setup_locale();
 
 		if ($order_id && !is_a($order, 'WC_Order')) {
 			$order = wc_get_order($order_id);
 		}
-
+		
 		if (is_a($order, 'WC_Order')) {
 			$this->object = $order;
 			$this->recipient = $this->object->get_billing_email();
 			$this->placeholders['{order_date}'] = wc_format_datetime($this->object->get_date_created());
 			$this->placeholders['{order_number}'] = $this->object->get_order_number();
 		}
-
+		
 		if ($this->is_enabled() && $this->get_recipient()) {
 			$this->send($this->get_recipient(), $this->get_subject(), $this->get_content(), $this->get_headers(), $this->get_attachments());
 		}
